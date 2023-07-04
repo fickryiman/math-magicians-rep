8b8bc2504c89b7c3ad20b7c131e89b277f2771cd
Fickry Iman <fickry.iman@gmail.com>
2023-07-04 17:00:54 +0700

    develop a React component that will hold the core functionality: a calculator
    - Delete all the boilerplate from Create React App (text, images, styles).
    - Create a new directory called components.
    - Inside components, create a new Calculator.js file.
    - In Calculator.js, create a React component with calculator design.
    - Develop the UI of the calculator; the math logic to make it work will be added in the next project.
    - import calculator component in top-level component: App.js

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,32 @@
+import './Calculator.css';
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <input type="text" className="calculator-screen" value={0} disabled />
+      <div className="calculator-keys">
+        <button type="button" className="all-clear" value="all-clear">AC</button>
+        <button type="button" className="plus-minus" value="plus-minus">+/-</button>
+        <button type="button" className="percent" value="percent">%</button>
+        <button type="button" className="operator" value="+">+</button>
+        <button type="button" value="7">7</button>
+        <button type="button" value="8">8</button>
+        <button type="button" value="9">9</button>
+        <button type="button" className="operator" value="*">&times;</button>
+        <button type="button" value="4">4</button>
+        <button type="button" value="5">5</button>
+        <button type="button" value="6">6</button>
+        <button type="button" className="operator" value="-">-</button>
+        <button type="button" value="1">1</button>
+        <button type="button" value="2">2</button>
+        <button type="button" value="3">3</button>
+        <button type="button" className="operator" value="/">&divide;</button>
+        <button type="button" className="zero" value="0">0</button>
+        <button type="button" className="decimal" value=".">.</button>
+        <button type="button" className="equal-sign" value="=">=</button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
