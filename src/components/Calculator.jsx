3f4f21fe676f164fef405ee90952b10b88f418cf
Fickry Iman <fickry.iman@gmail.com>
2023-07-05 22:23:04 +0700

    add calculator logic
    - Create a new directory called logic
    - Add calculate.js and operate.js into logic directory
    - Install big.js using NPM
    - Import calculate.js in your Calculator.js component
    - Implement click event handlers using the math logic from calculate.js and operate.js in React component

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,32 +1,45 @@
 import './Calculator.css';
+import React, { useState } from 'react';
 import Hello from './Hello';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, addState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const calc = (event) => {
+    const newState = calculate(state, event.target.textContent);
+    addState(newState);
+  };
+
   return (
     <>
       <Hello />
       <div className="calculator">
-        <input type="text" className="calculator-screen" value={0} disabled />
+        <input type="text" className="calculator-screen" value={state.next || state.total || 0} disabled />
         <div className="calculator-keys">
-          <button type="button" className="all-clear" value="all-clear">AC</button>
-          <button type="button" className="plus-minus" value="plus-minus">+/-</button>
-          <button type="button" className="percent" value="percent">%</button>
-          <button type="button" className="operator" value="+">+</button>
-          <button type="button" value="7">7</button>
-          <button type="button" value="8">8</button>
-          <button type="button" value="9">9</button>
-          <button type="button" className="operator" value="*">&times;</button>
-          <button type="button" value="4">4</button>
-          <button type="button" value="5">5</button>
-          <button type="button" value="6">6</button>
-          <button type="button" className="operator" value="-">-</button>
-          <button type="button" value="1">1</button>
-          <button type="button" value="2">2</button>
-          <button type="button" value="3">3</button>
-          <button type="button" className="operator" value="/">&divide;</button>
-          <button type="button" className="zero" value="0">0</button>
-          <button type="button" className="decimal" value=".">.</button>
-          <button type="button" className="equal-sign" value="=">=</button>
+          <button type="button" className="all-clear" value="all-clear" onClick={calc}>AC</button>
+          <button type="button" className="plus-minus" value="plus-minus" onClick={calc}>+/-</button>
+          <button type="button" className="percent" value="percent" onClick={calc}>%</button>
+          <button type="button" className="operator" value="+" onClick={calc}>+</button>
+          <button type="button" value="7" onClick={calc}>7</button>
+          <button type="button" value="8" onClick={calc}>8</button>
+          <button type="button" value="9" onClick={calc}>9</button>
+          <button type="button" className="operator" value="*" onClick={calc}>x</button>
+          <button type="button" value="4" onClick={calc}>4</button>
+          <button type="button" value="5" onClick={calc}>5</button>
+          <button type="button" value="6" onClick={calc}>6</button>
+          <button type="button" className="operator" value="-" onClick={calc}>-</button>
+          <button type="button" value="1" onClick={calc}>1</button>
+          <button type="button" value="2" onClick={calc}>2</button>
+          <button type="button" value="3" onClick={calc}>3</button>
+          <button type="button" className="operator" value="/" onClick={calc}>÷</button>
+          <button type="button" className="zero" value="0" onClick={calc}>0</button>
+          <button type="button" className="decimal" value="." onClick={calc}>.</button>
+          <button type="button" className="equal-sign" value="=" onClick={calc}>=</button>
         </div>
       </div>
     </>
