ff7940fc79ac9fe1b4211e13afbf8db818569d03
Fickry Iman <fickry.iman@gmail.com>
2023-07-12 21:01:28 +0700

    fixes linters error

--- src/tests/Quote.test.js
+++ src/tests/Quote.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import Quote from '../components/Quote';test('It should render correctly', () => {
+import Quote from '../components/Quote';
+
+test('It should render correctly', () => {
   const tree = render(
     <BrowserRouter>
       <Quote />
